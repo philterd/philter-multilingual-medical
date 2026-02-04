9c9c612a711597c86a8ca82cf50ded8b46b8013f
jzonthemtn <jeff.zemerick@mtnfog.com>
2026-02-04 10:05:36 -0500

    Updating policy and images.

--- test.sh
+++ test.sh
@@ -3,16 +3,11 @@
 # English
 
 echo "English test:"
-curl "http://localhost:8080/api/filter?p=english" \
-	--data "George Washington was president and his ssn was 123-45-6789 with diabetes and high blood pressure." \
-  -H "Content-type: text/plain"
-
+curl "http://localhost:8080/api/filter?p=english" --data "George Washington was president and his ssn was 123-45-6789 with diabetes and high blood pressure." -H "Content-type: text/plain"
 
 echo ""
 echo ""
 
 # French
 echo "French test:"
-curl "http://localhost:8080/api/filter?p=french" \
-	--data "George Washington was president and his ssn was 123-456-6789 with diabetes and high blood pressure." \
-  -H "Content-type: text/plain"
+curl "http://localhost:8080/api/filter?p=french" --data "Je m'appelle Mary et je suis diabétique." -H "Content-type: text/plain"
